2e55ea9d70c2b437225b8ad47d4b6711e87ccab2
Hussam <hussamcodeing@gmail.com>
2023-12-01 18:48:56 +0300

    FizzBuzz Game

--- Day 5/Day5.4.py
+++ Day 5/Day5.4.py
@@ -1,1 +1,10 @@
- 
+ #range() Funcation with looop   
+for n in range(0,10,2):
+     print(n)
+     
+#Give me the sum of even number from 1 - 100
+TotalEvenNumber = 0 
+for number in range(0,101):
+    if((number % 2 )== 0 ):
+        TotalEvenNumber += number
+print(TotalEvenNumber)
---
+++ Day 5/FizzBuzz/README.md
@@ -0,0 +1,2 @@
+# This is One of the most Interview Questions
+Which is FizzBuzz interview Questions cause it test how u think 
