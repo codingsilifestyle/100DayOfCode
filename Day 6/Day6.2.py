046076919909d5071cefc2b5f7a848521dbc3909
Hussam <hussamcodeing@gmail.com>
2023-12-29 06:22:02 +0300

    Function

---
+++ Day 6/Day6.2.py
@@ -0,0 +1,12 @@
+def greet(Name , Age , Place):
+    print(f"Hi Master {Name}")
+    print(f"Your Age Is {Age}")
+    print(f"You study in {Place}")
+    
+print("******************************************")
+print("            SignUp                        ")
+print("******************************************")
+Name = input("Enter Your Name:")
+Age = input("Enter Your Age :")
+Place = input("Enter the name of your Universty:")
+greet(Name , Age , Place)
